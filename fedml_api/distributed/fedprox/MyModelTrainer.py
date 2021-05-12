33de408ae67b0359b4dd301087ee71bd7d99f708
Ethan <yicruan@gmail.com>
2021-05-11 23:11:10 -0700

    deep copy

--- fedml_api/distributed/fedprox/MyModelTrainer.py
+++ fedml_api/distributed/fedprox/MyModelTrainer.py
@@ -48,11 +48,10 @@ class MyModelTrainer(ModelTrainer):
                 log_probs = model(x)
                 loss = criterion(log_probs, labels)
                 local_parameters=[]
-                for i in model.parameters():
+                for i in copy.deepcopy(model).parameters():
                     local_parameters.append(i)
                 l_index=0
                 for key in global_state_dict:
-                        print(args.mu)
                         loss += args.mu * torch.norm(local_parameters[int(l_index)]-global_state_dict[key])**2 #for rpi 4 pytorch 1.4.0
                         l_index+=1
 
